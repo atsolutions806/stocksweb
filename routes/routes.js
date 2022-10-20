4b799217a4b5c1b7b6fcdcf113c3bc512de85289
atsolutions806 <115154878+atsolutions806@users.noreply.github.com>
2022-10-20 12:11:18 -0400

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -1,15 +1,15 @@
 const express = require('express')
 const {protect}=require('../middleware/auth')
 const router = express.Router();
-const{searchData}=require('../controller/data.controller')
-router.route('/search').get(searchData)
+
 const{addAlert,alerts,deleteAlert,findUserAlert}=require('../controller/alert.controller')
-router.route('/addAlert').post(protect,addAlert)
+router.route('/addAlert').post(addAlert)
 router.route('/alerts').get(protect,alerts)
 router.route('/deleteAlert').delete(deleteAlert)
 router.route('/findUserAlert').get(protect,findUserAlert)
 const{addPortfolio,deletePortfolio}=require('../controller/portfolio.controller')
 router.route('/addPortfolio').post(protect,addPortfolio)
 router.route('/deletePortfolio').delete(protect,deletePortfolio)
-
+const{getStockData}=require('../controller/data.controller')
+router.route('/getStockData').get(getStockData)
 module.exports= router
